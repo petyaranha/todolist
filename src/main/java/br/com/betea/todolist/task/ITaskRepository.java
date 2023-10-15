e6dd351207909bc202497b499b425c3d9f261b29
Bete Aranha <petyaranha@terra.com.br>
2023-10-15 18:43:51 -0300

    Dockerfile deploy

---
+++ src/main/java/br/com/betea/todolist/task/ITaskRepository.java
@@ -0,0 +1,13 @@
+package br.com.betea.todolist.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
+
+}
